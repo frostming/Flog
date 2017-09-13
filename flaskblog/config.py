0fc3465b8be81060fe3f448d431e64a9017cd2e5
Frost Ming <mianghong@gmail.com>
2017-09-13 22:00:21 +0800

    allow override database uri by env

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,5 +1,6 @@
 """Flask blog app config"""
 import os.path as op
+import os
 
 here = op.dirname(__file__)
 
@@ -8,7 +9,9 @@ BLOG_THEME_NAME = 'footstrap'
 BLOG_THEME_PROCESSOR = 'site'
 SECRET_KEY = 'flask blog'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(here, 'db.sqlite3')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                    'sqlite:///' + op.join(here, 'db.sqlite3'))
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 BLOG_PER_PAGE = 10
