acfc2b4d4d687aa8d1d867529ce0e884e4664a34
Frost Ming <mianghong@gmail.com>
2017-09-16 09:01:23 +0800

    change to respect DB envs

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -9,8 +9,18 @@ BLOG_THEME_NAME = 'footstrap'
 BLOG_THEME_PROCESSOR = 'site'
 SECRET_KEY = 'flask blog'
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
-                                    'sqlite:///' + op.join(here, 'db.sqlite3'))
+# aws environment
+if 'RDS_HOSTNAME' in os.environ:
+    db_name = os.environ['RDS_DB_NAME']
+    db_user = os.environ['RDS_USERNAME']
+    db_password = os.environ['RDS_PASSWORD']
+    db_host = os.environ['RDS_HOSTNAME']
+    db_port = os.environ['RDS_PORT']
+    SQLALCHEMY_DATABASE_URI = ('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'
+                               .format(db_user, db_password, db_host, db_port,
+                                       db_name))
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(here, 'db.sqlite3')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
