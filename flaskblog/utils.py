cdf567b309592a1ea61cf1c3c6fea446670c1c15
Frost Ming <mianghong@gmail.com>
2017-10-22 11:08:39 +0800

    fix a bug

--- flaskblog/utils.py
+++ flaskblog/utils.py
@@ -5,6 +5,6 @@ from sqlalchemy import func
 def get_tag_cloud():
     """Get tags order by its heat to generate a tag cloud"""
     tags = Tag.query.join(Tag.posts).with_entities(Tag, func.count(Post.id))\
-                                    .group_by(Tag.text)\
+                                    .group_by(Tag.id)\
                                     .order_by(func.count(Post.id).desc())
     return tags.all()
