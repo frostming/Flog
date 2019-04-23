349297e15b484eccb0ddeadf2adb4d9c61d8940d
Frost Ming <mianghong@gmail.com>
2019-04-23 21:08:52 +0800

    rsync fix

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ set -ex
 
 docker tag flog $DOCKER_USERNAME/flog:latest
 docker push $DOCKER_USERNAME/flog:latest
-rsync -arz --delete ./static --include 'cards/' --include 'css/' --include 'js/' --include 'images/' \
-    --include 'iconfont/' --include "robots.txt" --include 'dist/' --exclude '/*' \
-    fming@frostming.com:/home/fming/frostming.com/
-rsync -arz ./nginx fming@frostming.com:/home/fming/frostming.com/
-rsync -az ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
+rsync -avz --delete static/ --include='cards/' --include='css/' --include='js/' --include='images/' \
+    --include='iconfont/' --include="robots.txt" --include='dist/' --exclude='/*' \
+    fming@frostming.com:/home/fming/frostming.com/static
+rsync -avz nginx/ fming@frostming.com:/home/fming/frostming.com/nginx
+rsync -avz ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
 ssh fming@frostming.com bash -s << EOF
 cd /home/fming/frostming.com
 docker-compose down
