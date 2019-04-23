97aba7bbeeddc5e56a396219a0bf4e6e89b48a5e
Frost Ming <mianghong@gmail.com>
2019-04-23 20:47:58 +0800

    rsync

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ set -ex
 
 docker tag flog $DOCKER_USERNAME/flog:latest
 docker push $DOCKER_USERNAME/flog:latest
-rsync -arz --delete ./static --include='cards/' --include='css/' --include='js/' --include='images/' \
-    --include='iconfont/' --include="robots.txt" --exclude='/*' \
+rsync -arz --delete ./static --include 'cards/' --include 'css/' --include 'js/' --include 'images/' \
+    --include 'iconfont/' --include "robots.txt" --include 'dist/' --exclude '/*' \
     fming@frostming.com:/home/fming/frostming.com/
 rsync -arz ./nginx fming@frostming.com:/home/fming/frostming.com/
 rsync -az ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
