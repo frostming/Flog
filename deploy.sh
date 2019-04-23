6fd5f3ca3e7d0c674166fa5d735356091c6c981c
Frost Ming <mianghong@gmail.com>
2019-04-23 20:32:01 +0800

    add -z flag

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ set -ex
 
 docker tag flog $DOCKER_USERNAME/flog:latest
 docker push $DOCKER_USERNAME/flog:latest
-rsync -ar --delete ./static --include='cards/' --include='css/' --include='js/' --include='images/' \
+rsync -arz --delete ./static --include='cards/' --include='css/' --include='js/' --include='images/' \
     --include='iconfont/' --include="robots.txt" --exclude='/*' \
     fming@frostming.com:/home/fming/frostming.com/static
-rsync -ar ./nginx fming@frostming.com:/home/fming/frostming.com/nginx
-rsync -a ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
+rsync -arz ./nginx fming@frostming.com:/home/fming/frostming.com/nginx
+rsync -az ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
 ssh fming@frostming.com bash -s << EOF
 docker-compose down
 docker-compose pull web
