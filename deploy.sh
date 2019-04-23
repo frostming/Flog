cf774ada97f4124867f1ae541567915b709120bb
Frost Ming <mianghong@gmail.com>
2019-04-23 20:32:40 +0800

    fix directory

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ docker tag flog $DOCKER_USERNAME/flog:latest
 docker push $DOCKER_USERNAME/flog:latest
 rsync -arz --delete ./static --include='cards/' --include='css/' --include='js/' --include='images/' \
     --include='iconfont/' --include="robots.txt" --exclude='/*' \
-    fming@frostming.com:/home/fming/frostming.com/static
-rsync -arz ./nginx fming@frostming.com:/home/fming/frostming.com/nginx
+    fming@frostming.com:/home/fming/frostming.com/
+rsync -arz ./nginx fming@frostming.com:/home/fming/frostming.com/
 rsync -az ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
 ssh fming@frostming.com bash -s << EOF
 docker-compose down
