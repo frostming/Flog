2efe2b23f990e5b221b8aa01fd641a3a696f7c49
Frost Ming <mianghong@gmail.com>
2019-04-23 17:39:27 +0800

    change ci script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,11 @@ set -ex
 
 docker tag flog $DOCKER_USERNAME/flog:latest
 docker push $DOCKER_USERNAME/flog:latest
-ssh fming@frostming.com make -C /home/fming/frostming.com
+rsync -aPr --delete ./static fming@frostming.com:/home/fming/frostming.com/static
+rsync -aPr ./nginx fming@frostming.com:/home/fming/frostming.com/nginx
+rsync -aP ./docker-compose.yml fming@frostming.com:/home/fming/frostming.com/
+ssh fming@frostming.com bash -s << EOF
+docker-compose down
+docker-compose pull web
+docker-compose up -d
+EOF
