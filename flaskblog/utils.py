cff0c091626061e734f77f219c32b3a4128047f6
Frost Ming <mianghong@gmail.com>
2020-03-07 09:05:58 +0800

    :bug: strip tags

--- flaskblog/utils.py
+++ flaskblog/utils.py
@@ -2,6 +2,6 @@ import re
 
 
 def strip_tags(text: str) -> str:
-    text = re.sub(r"<.+?>\s*<.+?>", "", text, flags=re.DOTALL)
-    text = re.sub(r"<.+?>", "", text, flags=re.DOTALL)
-    return text
+    text = re.sub(r"<[^<>]+?>\s*(?=<[^<>]+?>)", "", text, flags=re.DOTALL)
+    text = re.sub(r"<[^<>]+?>", "", text, flags=re.DOTALL)
+    return text.strip()
