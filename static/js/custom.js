d29985da3fd3826cd58b99dfed5be2626e2960df
Frost Ming <mianghong@gmail.com>
2020-03-10 16:33:00 +0800

    :fire: remove unused plugins

--- static/js/custom.js
+++ static/js/custom.js
@@ -22,56 +22,8 @@
         menu.classList.remove('open')
       })
     }
-  })
-
-  $('.owl-carousel').owlCarousel({
-    items: 4,
-    lazyLoad: true,
-    loop: true,
-    dots: true,
-    margin: 30,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
-    }
-  })
-
-  $('.hover').mouseleave(function() {
-    $(this).removeClass('hover')
-  })
-
-  $('.isotope-wrapper').each(function() {
-    var $isotope = $('.isotope-box', this)
-    var $filterCheckboxes = $('input[type="radio"]', this)
-
-    var filter = function() {
-      var type = $filterCheckboxes.filter(':checked').data('type') || '*'
-      if (type !== '*') {
-        type = '[data-type="' + type + '"]'
-      }
-      $isotope.isotope({ filter: type })
-    }
-
-    $isotope.isotope({
-      itemSelector: '.isotope-item',
-      layoutMode: 'masonry'
-    })
-
-    $(this).on('change', filter)
-    filter()
-  })
-  lightbox.option({
-    resizeDuration: 200,
-    wrapAround: true
   });
+
   (function() {
     var $pswp = $('.pswp')
     if ($pswp.length === 0) return
