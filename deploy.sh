c1a98d9a142f090a32662e4d18b2863af298334f
Frost Ming <mianghong@gmail.com>
2020-03-16 18:21:44 +0800

    :bug: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ rsync -avz --delete static/ --include='cards/' --include='css/' --include='js/' 
     --include='iconfont/' --include="robots.txt" --include='dist/' --exclude='/*' \
     fming@frostming.com:/home/fming/frostming.com/static
 rsync -avz nginx/ fming@frostming.com:/home/fming/frostming.com/nginx
-rsync -avz --delete --exclude='*.pyc' Pipfile* ./docker-compose.yml Dockerfile flaskblog migrations start_server.sh fming@frostming.com:/home/fming/frostming.com/
+rsync -avz --delete --exclude='*.pyc' pyproject.toml pdm.lock ./docker-compose.yml Dockerfile \
+    flaskblog migrations start_server.sh fming@frostming.com:/home/fming/frostming.com/
 ssh fming@frostming.com bash -s << EOF
 cd /home/fming/frostming.com
 sudo docker-compose build web
