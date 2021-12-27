1ef4f82ab7e106c01e00f8ef0ded8e7f79b870b2
Frost Ming <mianghong@gmail.com>
2021-12-27 09:08:41 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
-PYPACKAGES=$(pdm info --packages)
-export PATH=$PYPACKAGES/bin:$PATH
-eval $(pdm --pep582)
 
-FLASK_APP=flaskblog.app flask db upgrade
-pybabel compile -d flaskblog/translations
+FLASK_APP=flaskblog.app pdm run flask db upgrade
+pdm run pybabel compile -d flaskblog/translations
 PORT=${PORT:-5000}
 
-exec gunicorn -b :${PORT} --access-logfile - --error-logfile - -k gevent -w 4 "flaskblog.app:create_app()"
+exec pdm run gunicorn -b :${PORT} --access-logfile - --error-logfile - -k gevent -w 4 "flaskblog.app:create_app()"
